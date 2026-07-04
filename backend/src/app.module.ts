f12a6392ec2b1e8c9cf3e755418433be5779d3fd
Pierre <pierre.laffaille@outlook.fr>
2026-07-04 14:09:42 +0200

    feat: add prisma exception filter

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -4,10 +4,15 @@ import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
 import { AuthModule } from './auth/auth.module';
 import { PrismaModule } from 'prisma/prisma.module';
+import { APP_FILTER } from '@nestjs/core';
+import { PrismaExceptionFilter } from './common/filters/prisma-exception.filter';
 
 @Module({
   imports: [UsersModule, AuthModule, PrismaModule],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [
+    AppService,
+    { provide: APP_FILTER, useClass: PrismaExceptionFilter },
+  ],
 })
 export class AppModule {}
