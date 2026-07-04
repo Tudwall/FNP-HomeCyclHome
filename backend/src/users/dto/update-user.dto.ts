b88339f9981c6e3b6a2caa27ee839a5ba349d6d5
Pierre <pierre.laffaille@outlook.fr>
2026-07-04 13:57:45 +0200

    feat: base user crud

--- backend/src/users/dto/update-user.dto.ts
+++ backend/src/users/dto/update-user.dto.ts
@@ -1,9 +1,6 @@
-import { PartialType } from '@nestjs/mapped-types';
+import { PartialType, OmitType } from '@nestjs/mapped-types';
 import { CreateUserDto } from './create-user.dto';
-import { IsString, MaxLength } from 'class-validator';
 
-export class UpdateUserDto extends PartialType(CreateUserDto) {
-    @IsString()
-    @MaxLength(100)
-    first_name: string
-}
+export class UpdateUserDto extends PartialType(
+  OmitType(CreateUserDto, ['password'] as const),
+) {}
--- backend/src/users/users.service.ts
+++ backend/src/users/users.service.ts
@@ -1,8 +1,12 @@
-import { Injectable } from '@nestjs/common';
+import {
+  ConflictException,
+  Injectable,
+  NotFoundException,
+} from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
 import { PrismaService } from 'prisma/prisma.service';
-import { AppUser } from '../generated/prisma/client';
+import { AppUser, Prisma } from '../generated/prisma/client';
 
 @Injectable()
 export class UsersService {
@@ -13,22 +17,50 @@ export class UsersService {
   }
 
   findAll() {
-    return this.prisma.appUser.findMany();
+    return this.prisma.appUser.findMany({ where: { deletedOn: null } });
   }
 
   findById(id: number) {
-    return this.prisma.appUser.findUnique({ where: { id } });
+    return this.prisma.appUser.findUnique({ where: { id, deletedOn: null } });
   }
 
   findByEmail(email: string): Promise<AppUser | null> {
     return this.prisma.appUser.findUnique({ where: { email } });
   }
 
-  update(id: number, updateUserDto: UpdateUserDto) {
-    return `This action updates a #${id} user`;
+  update(id: number, updateUserDto: UpdateUserDto): Promise<appUser> {
+    try {
+      return this.prisma.app_user.update({
+        where: { id },
+        data: updateUserDto,
+      });
+    } catch (err) {
+      if (err instanceof Prisma.PrismaClientKnownRequestError) {
+        if (err.code === 'P2025') {
+          throw new NotFoundException(`User #${id} not fount`);
+        }
+        if (err.code === 'P2002') {
+          throw new ConflictException('Conflict');
+        }
+      }
+      throw err;
+    }
   }
 
-  remove(id: number) {
-    return `This action removes a #${id} user`;
+  remove(id: number): Promise<appUser> {
+    try {
+      return this.prisma.app_user.update({
+        where: { id },
+        data: { deletedOn: new Date() },
+      });
+    } catch (err) {
+      if (
+        err instanceof Prisma.PrismaClientKnownRequestError &&
+        err.code === 'P2025'
+      ) {
+        throw new NotFoundException(`User #${id} not found`);
+      }
+      throw err;
+    }
   }
 }
