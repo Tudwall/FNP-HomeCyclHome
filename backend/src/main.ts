ecbe03ffaed2c84d6c1ca4af4341a24214b5cbfe
Pierre <pierre.laffaille@outlook.fr>
2026-08-08 20:07:45 +0200

    fix(backend): repair docker build and runtime config

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,5 +1,6 @@
 import { NestFactory } from '@nestjs/core';
 import { configureApp } from './configure-app';
+import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
